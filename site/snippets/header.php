3ee849dc8ae437372d5f2f2e604a2646bdcecac9
Eric Li <ericyoungli@gmail.com>
2019-09-13 22:50:06 -0400

    Remove some kirby stuff in header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,15 +14,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <!-- The title tag we show the title of our site and the title of the current page -->
   <title><?= $page->title() ?> | <?= $site->title() ?></title>
 
   <?php if ($about = page('about')): ?>
     <link rel="shortcut icon" type="image/png" href="<?= $about->favicon()->toFile()->url(); ?>"/>
   <?php endif; ?>
 
-  <!-- Stylesheets can be included using the `css()` helper. Kirby also provides the `js()` helper to include script file.
-        More Kirby helpers: https://getkirby.com/docs/reference/templates/helpers -->
   <?= css(['assets/css/type.css', 'assets/css/index.css', 'assets/css/works_grid.css', 'assets/css/installation_images_grid.css', '@auto']) ?>
   <?= js(['assets/js/main.js', '@auto']) ?>
 
@@ -40,11 +37,7 @@
   <header id="header" class="header">
     <ul id="navigation" class="nav">
       <li class="nav__item"><a class="nav__item__link" href="<?= $site->url() ?>"><?= $site->title() ?></a></li>
-      <?php
-      // In the menu, we only fetch listed pages, i.e. the pages that have a prepended number in their foldername
-      // We do not want to display links to unlisted `error`, `home`, or `sandbox` pages
-      // More about page status: https://getkirby.com/docs/reference/panel/blueprints/page#statuses
-      foreach ($site->children()->listed() as $item): ?>
+      <?php foreach ($site->children()->listed() as $item): ?>
         <li class="nav__item"><a class="nav__item__link" href="<?= $item->url() ?>"><?= $item->title() ?></a></li>
       <?php endforeach ?>
     </nav>
