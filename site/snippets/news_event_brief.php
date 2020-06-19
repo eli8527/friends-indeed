52baee400cf9ba4856f2a0c5590264908268f630
Eric Li <ericyoungli@gmail.com>
2020-06-19 10:29:07 -0400

    update event brief

--- site/snippets/news_event_brief.php
+++ site/snippets/news_event_brief.php
@@ -1,9 +1,11 @@
 <li class="news_event__brief center max-width">
-  <a href="<?= $news_event->url(); ?>">
-    <div><?= $news_event->title() ?></div><div class="secondary"><?php snippet('news_event_datetime', ['news_event' => $news_event]); ?></div><p></p>
+    <a href="<?= $news_event->url(); ?>">
+      <div><?= $news_event->title() ?></div>
+      <div class="secondary"><?php snippet('news_event_datetime', ['news_event' => $news_event]); ?></div>
+      <p></p>
+    </a>
     <div class="text">
       <?= $news_event->intro()->kt() ?>
       <p></p>
     </div>
-  </a>
 </li>
