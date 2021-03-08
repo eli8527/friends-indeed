266988084241ebacc39838ff794042e9539f83ee
Eric Li <ericyoungli@gmail.com>
2021-03-08 08:46:31 -0500

    ibid

---
+++ site/snippets/email-signup.php
@@ -0,0 +1,14 @@
+<form action="https://art.us4.list-manage.com/subscribe/post?u=3e0d4ce886f122a5bd36bb0cf&amp;id=186cccf9d9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+  <div id="mc_embed_signup_scroll" style="display: flex;">
+  <div class="mc-field-group">
+    <label for="mce-EMAIL"></label>
+    <input type="email" placeholder="Sign up for updates" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+  </div>
+  <div id="mce-responses" class="clear">
+    <div class="response" id="mce-error-response" style="display:none"></div>
+    <div class="response" id="mce-success-response" style="display:none"></div>
+  </div>
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3e0d4ce886f122a5bd36bb0cf_186cccf9d9" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" style="visibility: hidden; width: 0; height: 0; overflow: hidden;"></div>
+  </div>
+</form>
